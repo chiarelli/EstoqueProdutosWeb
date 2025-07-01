799ad3fa69dc9105c0d4ce9580735aa71f864e71
Raphael Mathias <oi.sucesso@gmail.com>
2025-07-01 19:50:03 +0000

    Feedback message after product edited

--- src/app/pages/produto-edit/produto-edit.component.ts
+++ src/app/pages/produto-edit/produto-edit.component.ts
@@ -1,21 +1,31 @@
-import { Produto, ProdutoRequest } from './../../interfaces';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProdutoFormComponent, SubmitResponse as SubmitRequest } from 'src/app/components/produto-form/produto-form.component';
+import { ErrosAPIResponse } from 'src/app/dtos/errors-response';
 import { ProdutosService } from 'src/app/services/produtos.service';
+import { ItemFeedbackComponent } from "../../components/item-feedback/item-feedback.component";
+import { Produto, ProdutoRequest } from './../../interfaces';
 
 @Component({
   selector: 'app-produto-edit',
   imports: [
+    CommonModule,
     ProdutoFormComponent,
-    RouterLink
-  ],
+    RouterLink,
+    ItemFeedbackComponent
+],
   templateUrl: './produto-edit.component.html',
   styleUrl: './produto-edit.component.scss'
 })
 export class ProdutoEditComponent implements OnInit {
 
+  private readonly msgSuccessTemplate: string = 'Produto $1 atualizado com sucesso!';
+
+  apiErrors = new ErrosAPIResponse({});
+  msgSuccess: string = '';
   produto?: Produto;
+  produtoAtualizado?: Produto;
 
   constructor(
     private produtosService: ProdutosService,
@@ -47,13 +57,17 @@ export class ProdutoEditComponent implements OnInit {
     const { payload } = formValue;
     const data = { ...payload } as ProdutoRequest;
 
+    this.msgSuccess = '';
+    this.apiErrors = new ErrosAPIResponse({});
+
     this.produtosService.atualizar(this.produto?.id as string, data).subscribe({
       next: (value) => {
-        const Produto = { ...value } as Produto;
-        this.produto = Produto;
+        const produto = { ...value } as Produto;
+        this.produto = this.produtoAtualizado = {...produto} as Produto;
+        this.msgSuccess = this.msgSuccessTemplate.replace('$1', produto.nome);
       },
       error: (err) => {
-        alert('Ocorreu um erro ao atualizar o produto');
+        this.apiErrors = new ErrosAPIResponse(err.error);
         console.error(err);
       }
     });
